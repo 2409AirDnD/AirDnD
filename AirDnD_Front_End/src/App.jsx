770c2c68ec38931e36c0e55a9e3c56bb115a1833
Scott <Scottyjmt13@gmail.com>
2024-12-06 11:49:03 -0700

    Typo - Remove '.jsx' from register route element.

--- AirDnD_Front_End/src/App.jsx
+++ AirDnD_Front_End/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element= {<LandingPage />} />
       <Route path="/charactersheet" element= {<CharacterSheet />} />
-      <Route path="/register" element= {<Register.jsx />} />
+      <Route path="/register" element= {<Register />} />
     </Routes>
     </>
   )
