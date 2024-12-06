231eb30d2b362b1c08db724136917cbabec4a33d
Jared Mc Allister <jaredmcallister@Jareds-Laptop.attlocal.net>
2024-12-06 13:07:32 -0600

    Added the label to the avatar to give it a text box in register.jsx, added a return in login.jsx and renamed it to Login.jsx and moved it to the compoent folder.

--- AirDnD_Front_End/src/components/Register.jsx
+++ AirDnD_Front_End/src/components/Register.jsx
@@ -7,42 +7,44 @@ const Register = () => {
   //const [token, setToken] = useState('')
   //const handleSubmit = async (event) => {
   //event.preventDefault()
-
-  <>
-    <h2>Register</h2>
-    <form onSubmit={(event) => handleSubmit(event)}>
-      <label>
-        Username:
+  return (
+    <>
+      <h2>Register</h2>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <label>
+          Username:
+          <input
+            onChange={(event) => setUsername(event.target.value)}
+            type="text"
+            username="username"
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            onChange={(event) => setEmail(event.target.value)}
+            type="email"
+            name="email"
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            onChange={(event) => setPassword(event.target.value)}
+            type="password"
+            name="password"
+          />
+        </label>
+        <label>Avatar:</label>
         <input
-          onChange={(event) => setUsername(event.target.value)}
+          onChange={(event) => setAvatar(event.target.value)}
           type="text"
-          username="username"
+          name="upload avatarURL"
         />
-      </label>
-      <label>
-        Email:
-        <input
-          onChange={(event) => setEmail(event.target.value)}
-          type="email"
-          name="email"
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          onChange={(event) => setPassword(event.target.value)}
-          type="password"
-          name="password"
-        />
-      </label>
-      <input
-        onChange={(event) => setAvatar(event.target.value)}
-        type="url"
-        name="upload avatarURL"
-      />
-      <input type="submit" value="Submit" />
-    </form>
-  </>;
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
 };
 //}
 
