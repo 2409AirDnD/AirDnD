dec2e6fbf9af7a9400b9a7f8fb24ef315478b2be
Scott <Scottyjmt13@gmail.com>
2024-12-08 19:41:05 -0700

    Add Proficiency Bonus div to skills.jsx and render to character sheet.

--- AirDnD_Front_End/src/components/Skills.jsx
+++ AirDnD_Front_End/src/components/Skills.jsx
@@ -1,8 +1,15 @@
 function Skills () {
   return (
+    <>
     <div id="skills-block">
       <h1>Skills.jsx</h1>
     </div>
+
+    <div id="proficiency-bonus-block">
+      <h2>Skills.jsx</h2>
+    </div>
+
+    </>
 	)
 }; 
 
