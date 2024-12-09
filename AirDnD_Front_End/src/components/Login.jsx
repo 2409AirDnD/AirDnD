2ff0406236a7c68270dc254314e9871108c1754b
Scott <Scottyjmt13@gmail.com>
2024-12-09 14:51:19 -0700

    Add Login Functionality. Style

--- AirDnD_Front_End/src/components/Login.jsx
+++ AirDnD_Front_End/src/components/Login.jsx
@@ -3,31 +3,74 @@ import { useState } from "react";
 function Login({ setToken }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loginToken, setLoginToken] = useState("");
+  const [loginSuccess, setLoginSuccess] = useState(false);
   const navigate = useNavigate();
-  const handleSubmit = async (event) => {
+
+  const login = async (event) => {
+
     event.preventDefault();
-  };
+
+    try {
+
+      const postCredentials = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json",},
+        body: JSON.stringify({
+          username: username,
+          password: password
+        }),
+      });
+
+      if (!postCredentials.ok) {
+        throw new Error("Invalid Credentials")
+      }
+
+      const responseJSON = await postCredentials.json();
+      const token = responseJSON.token;
+      setLoginToken(token);
+      setLoginSuccess(true);
+      setTimeout(() => {
+         navigate("/");
+         }, 3500);
+
+
+    } catch (e) {
+      alert(e.message || "An error occurred. Please try again."); 
+    }
+  }
 
   return (
     <>
-      <h2>Login</h2>
-      <form onSubmit={(event) => handleSubmit(event)}>
-        <label>
-          Username:
-          <input
-            onChange={(event) => setUsername(event.target.value)}
-            type="text"
-            name="username"
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            onChange={(event) => setPassword(event.target.value)}
-            type="password"
-            name="password"
-          />
-        </label>
+      <h2 id="login-header">Login</h2>
+      <form id="login-form" onSubmit={ login }>
+      {loginSuccess ? (
+        <p id="login-thank-you">Thank you for logging in. You will be redirected shortly.</p>
+        ) :(
+        <>
+          <div id="login-inputs">
+            <label>
+              Username:
+              <input
+                onChange={(event) => setUsername(event.target.value)}
+                type="text"
+                name="username"
+                required
+              />
+            </label>
+            <label>
+              Password:
+              <input
+                onChange={(event) => setPassword(event.target.value)}
+                type="password"
+                name="password"
+                required
+              />
+            </label>
+            </div>
+            <button>Log In</button>
+          </>
+        )}
       </form>
     </>
   );
