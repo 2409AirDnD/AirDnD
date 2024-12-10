5053ef37690d04e67041c2f831c6d5e72ed9a113
Scott Minnick <scottyjmt13@gmail.com>
2024-12-10 10:06:24 -0700

    un-comment-out cors lines

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-//app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use(require("./api/auth").router);
 
