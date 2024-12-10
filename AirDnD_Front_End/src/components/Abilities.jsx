0db19fc41a795d8298fb729736b454efae870b7f
Scott <Scottyjmt13@gmail.com>
2024-12-10 13:03:43 -0700

    Add individual ability divs

--- AirDnD_Front_End/src/components/Abilities.jsx
+++ AirDnD_Front_End/src/components/Abilities.jsx
@@ -1,7 +1,49 @@
 const Abilities = () => {
   return (
     <div id="abilities-block">
-      <h1>Abilities.jsx</h1>
+
+      <div className="single-ability-section">
+        <p className="ability-name">Stength</p>
+        <section className="ability-roll">
+
+        </section>
+      </div>
+
+      <div className="single-ability-section">
+        <p className="ability-name">Dexterity</p>
+        <section className="ability-roll">
+          
+        </section>
+      </div>
+
+      <div className="single-ability-section">
+        <p className="ability-name">Constitution</p>
+        <section className="ability-roll">
+          
+        </section>
+      </div>
+
+      <div className="single-ability-section">
+        <p className="ability-name">Intellegence</p>
+        <section className="ability-roll">
+          
+        </section>
+      </div>
+
+      <div className="single-ability-section">
+        <p className="ability-name">Wisdom</p>
+        <section className="ability-roll">
+          
+        </section>
+      </div>
+
+      <div className="single-ability-section">
+        <p className="ability-name">Charisma</p>
+        <section className="ability-roll">
+          
+        </section>
+      </div>
+
     </div>
   )
 }
