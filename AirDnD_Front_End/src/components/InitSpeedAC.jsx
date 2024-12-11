d0324e920c3720379e8436a574e8ce01646e1345
Scott <Scottyjmt13@gmail.com>
2024-12-11 11:57:22 -0700

    Style InitSpeedAC

--- AirDnD_Front_End/src/components/InitSpeedAC.jsx
+++ AirDnD_Front_End/src/components/InitSpeedAC.jsx
@@ -2,15 +2,15 @@ function InitSpeedAC() {
   return (
     <>
       <div id="init-block">
-        <h1>InitSpeedAC.jsx</h1>
+        <h1>Init</h1>
       </div>
 
       <div id="speed-block">
-        <h1>InitSpeedAC.jsx</h1>
+        <h1>Speed</h1>
       </div>
 
       <div id="ac-block">
-        <h1>InitSpeedAC.jsx</h1>
+        <h1>Armor Class</h1>
       </div>
     </>
   );
