6d82dcc8cd7b279a17173217b5ceb31e7215903f
Scott <Scottyjmt13@gmail.com>
2024-12-12 10:42:17 -0700

    fixed init calculation

--- AirDnD_Front_End/src/components/InitSpeedAC.jsx
+++ AirDnD_Front_End/src/components/InitSpeedAC.jsx
@@ -1,14 +1,10 @@
 const InitSpeedAC = ( { abilityModifiers } ) => {
 
-  const roll = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-  
   return (
     <div id="initspeedac-container">
       <div id="init-block">
         <h1 className="initspeedac-header">Initiative</h1>
-        <h2 className="initspeedac-value">{roll(1, 20) + abilityModifiers.dexterity}</h2>
+        <h2 className="initspeedac-value">{abilityModifiers.dexterity}</h2>
       </div>
 
       <div id="speed-block">
